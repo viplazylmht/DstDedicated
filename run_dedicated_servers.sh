996198823ad9656060e57ed93fb27e891087feab
viplazylmht <viplazylmht@gmail.com>
2021-06-29 07:35:48 +0700

    Add shell script file to auto run dedicated server

---
+++ run_dedicated_servers.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Send DST_DIR to 1st parameter of this shellcode
+# Send Cluster name to 2nd param, default to MyDediServer
+
+DST_DIR=$1
+cluster_name=$2
+steamcmd_dir="$DST_DIR/steamcmd"
+install_dir="$DST_DIR/dontstarvetogether_dedicated_server"
+#cluster_name="MyDediServer"
+dontstarve_dir="$DST_DIR/DoNotStarveTogether"
+
+function fail()
+{
+	echo Error: "$@" >&2
+	exit 1
+}
+
+function check_for_file()
+{
+	if [ ! -e "$1" ]; then
+		fail "Missing file: $1"
+	fi
+}
+
+cd "$steamcmd_dir" || fail "Missing $steamcmd_dir directory!"
+
+check_for_file "steamcmd.sh"
+check_for_file "$dontstarve_dir/$cluster_name/cluster.ini"
+check_for_file "$dontstarve_dir/$cluster_name/cluster_token.txt"
+check_for_file "$dontstarve_dir/$cluster_name/Master/server.ini"
+check_for_file "$dontstarve_dir/$cluster_name/Caves/server.ini"
+
+./steamcmd.sh +force_install_dir "$install_dir" +login anonymous +app_update 343050 validate +quit
+
+check_for_file "$install_dir/bin"
+
+cd "$install_dir/bin" || fail
+
+run_shared=(./dontstarve_dedicated_server_nullrenderer)
+run_shared+=(-console)
+run_shared+=(-cluster "$cluster_name")
+run_shared+=(-monitor_parent_process $$)
+
+"${run_shared[@]}" -shard Caves  | sed 's/^/Caves:  /' &
+"${run_shared[@]}" -shard Master | sed 's/^/Master: /'
+
